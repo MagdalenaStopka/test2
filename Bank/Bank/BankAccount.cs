228029dd91ef76af6ccde7cb8bf73d9ccbf3ece0
WSEI\m.stopka <magdalenaagh@gmail.com>
2018-09-25 20:24:44 +0200

    Dodanie następnego testu

--- Bank/Bank/BankAccount.cs
+++ Bank/Bank/BankAccount.cs
@@ -51,7 +51,7 @@ namespace BankAccountNS
             {
                 throw new ArgumentOutOfRangeException("amount");
             }
-            m_balance += amount;
+            m_balance -= amount;
         }
 
         public void Credit(double amount)
--- Bank/BankTests/BankTests.cs
+++ Bank/BankTests/BankTests.cs
@@ -9,6 +9,8 @@ namespace BankTests
     {
        [TestMethod]
 
+       
+
        public void Debit_WithValidAmount_UpdateBalance()
         {
             double beginningBalance = 11.99;
@@ -17,10 +19,22 @@ namespace BankTests
             BankAccount account = new BankAccount("Mr Bean ", beginningBalance);
 
             account.Debit(debitAmount);
+
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
 
             
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfException()
+        {
+            double beginningBalance = 11.99;
+            double DebitAmount = 100.00;
+            BankAccount account = new BankAccount(" mr Bean", beginningBalance);
+            account.Debit(DebitAmount);
+
         }
     }
 }
