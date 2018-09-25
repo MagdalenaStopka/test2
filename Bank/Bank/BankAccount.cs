9ebbdad79cdbb0f563629f64b16b87b3f3e2b11a
WSEI\m.stopka <magdalenaagh@gmail.com>
2018-09-25 19:41:51 +0200

    Dokończenie programu

--- Bank/Bank/BankAccount.cs
+++ Bank/Bank/BankAccount.cs
@@ -54,5 +54,42 @@ namespace BankAccountNS
             m_balance += amount;
         }
 
-    }
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+
+            {
+                throw new Exception(" Accout frozen");
+            }
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount(" Mr Bean", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}",ba.Balance );
+
+        }
+
+    }   
 }
